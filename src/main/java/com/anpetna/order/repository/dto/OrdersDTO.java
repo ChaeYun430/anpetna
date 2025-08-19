2f3eab59f12b94096c4c264f43ddf826759ea61f
KIMSUA <jjj2690jj@gmail.com>
2025-08-19 11:59:18 +0900

    DTO생성중2

---
+++ src/main/java/com/anpetna/order/repository/dto/OrdersDTO.java
@@ -0,0 +1,15 @@
+package com.anpetna.order.repository.dto;
+
+public class OrdersDTO {
+
+    private Long ordersId;
+
+    private String memberId;
+
+    private String cardId;
+
+    private int totalAmount;
+
+    private int itemCount;
+
+}
