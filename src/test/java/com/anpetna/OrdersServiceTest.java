b9209b374686ab72f95e4c8217494df1bf2ee2c9
KIMSUA <jjj2690jj@gmail.com>
2025-08-20 15:35:01 +0900

    테스트코드 확인 완료, 전체 코드 다듬음

--- src/test/java/com/anpetna/OrdersServiceTest.java
+++ src/test/java/com/anpetna/OrdersServiceTest.java
@@ -25,7 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
-
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
